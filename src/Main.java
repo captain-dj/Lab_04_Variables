77476a8ccb3f4e92d390bbad2f1c470ff0f2a194
captain-dj <whardig10@gmail.com>
2023-09-14 14:56:26 -0400

    V2

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        //intOperandA, intOperandB, intSum, intProduct, intDifference, intQuotient, intModulo
+
+        int intOperandA = 4;
+        int intOperandB = 7;
+
+        int intSum = 0;
+        int intProduct = 0;
+        int intDiffrance = 0;
+        int intQuotient = 0;
+        int intModulo = 0;
+
+        intSum = intOperandA + intOperandB;
+        System.out.println("The sum of "+ intOperandA +" and "+ intOperandB +" is " + intSum);
+
+        intProduct = intOperandA * intOperandB;
+        System.out.println("The product of "+ intOperandA +" and "+ intOperandB +" is " + intProduct);
+
+        // Do the rest of the int statents here
+
+        double doubleOperandA = 3.14;
+        double doubleOperandB = 3.50;
+        double doubleSum = 0;
+        double doubleProduct = 0;
+
+        doubleSum = doubleOperandA + doubleOperandB;
+        System.out.println("The sum of "+ doubleOperandA +" and "+ doubleOperandB +" is " + intSum);
+
+        doubleProduct = doubleOperandA * doubleOperandB;
+        System.out.println("The product of "+ doubleOperandA +" and "+ doubleOperandB +" is " + intProduct);
+
+    }
+}
